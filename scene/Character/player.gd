bf25a3c9354245a74c8174649dacefc54a56f3ba
Ragul <43083186+Ragul-R@users.noreply.github.com>
2024-06-07 22:35:30 +0530

    initial setup

---
+++ scene/Character/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+var speed = 350  # move speed in pixels/sec
+var rotation_speed = 1.5  # turning speed in radians/sec
+
+func _physics_process(delta):
+	var move_input = Input.get_axis("down", "up")
+	var rotation_direction = Input.get_axis("left", "right")
+	velocity = velocity.lerp(transform.x * move_input * speed, 0.05)
+	rotation += rotation_direction * rotation_speed * delta
+	move_and_slide()
