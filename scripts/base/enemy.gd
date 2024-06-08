e1ffad1c1f14ea4f794dc2ff81b13add869565fc
praveenpkg8 <praveepkg8@gmail.com>
2024-06-09 00:10:00 +0530

    Modified: rotation of enemy

--- scripts/base/enemy.gd
+++ scripts/base/enemy.gd
@@ -2,6 +2,7 @@ class_name Enemy
 extends Playable
 
 @export var attraction_force: float = 40.0
+@export var rotation_speed: float = 70.0
 var chase_player: bool = false
 
 func manage_attack():
@@ -21,7 +22,8 @@ func move_towards_player(
 
 	var attraction_velocity = direction * attraction_force
 	global_position += attraction_velocity * delta 
-	rotation = direction.angle()
+	var target_rotation = direction.angle()
+	rotation = lerp_angle(rotation, target_rotation, rotation_speed * delta)
 	
 	return {
 		"global_position": global_position, 
