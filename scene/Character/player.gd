02400eb68b211a3e882bc9dfc13bc14bdb67e2e2
Ragul <43083186+Ragul-R@users.noreply.github.com>
2024-06-09 11:24:31 +0530

    gappling hook

--- scene/Character/player.gd
+++ scene/Character/player.gd
@@ -1,11 +1,42 @@
 extends CharacterBody2D
 
-var speed = 350  # move speed in pixels/sec
+var speed = 50  # move speed in pixels/sec
 var rotation_speed = 1.5  # turning speed in radians/sec
+var time = 0
+var hooked: bool = false
+var radius
+var anchor_point
+var angle
 
 func _physics_process(delta):
-	var move_input = Input.get_axis("down", "up")
-	var rotation_direction = Input.get_axis("left", "right")
-	velocity = velocity.lerp(transform.x * move_input * speed, 0.05)
-	rotation += rotation_direction * rotation_speed * delta
+	if not hooked:
+	#var move_input = Input.get_axis("down", "up")
+		var rotation_direction = Input.get_axis("left", "right")
+		velocity = velocity.lerp(transform.x * 1 * speed, 0.05)
+		rotation += rotation_direction * rotation_speed * delta
+	else:
+		#var radius = global_position - hook_position
+		#var angle = acos(radius.dot(velocity) / (radius.length() * velocity.length()))
+		#var rad_vel = cos(angle) * velocity.length()
+		#velocity += radius.normalized() * -rad_vel
+		#velocity += (hook_position - global_position).normalized() * 15000 * delta
+		#velocity *= 0.975
+		time += delta
+		var rotation = Vector2(cos(angle), sin(angle))
+		position = rotation * radius + anchor_point
+		angle = speed * time
 	move_and_slide()
+
+
+func _on_chain_hooked(dist, pos):
+	hooked = true
+	speed = 1
+	time = 0
+	radius = dist
+	anchor_point = pos
+
+
+func _on_chain_un_hooked():
+	time = 0
+	speed = 50
+	hooked = false
---
+++ scene/Projectile/Chain.gd
@@ -0,0 +1,42 @@
+extends Sprite2D
+@onready var ray_cast = $RayCast2D
+var distance: float = 150.0
+ 
+signal hooked(hooked_position)
+signal un_hooked()
+var is_colling: bool = false
+ 
+func interpolate(length, duration = 0.2):
+	var tween_offset = get_tree().create_tween()
+	var tween_rect_h = get_tree().create_tween()
+ 
+	tween_offset.tween_property(self, "offset",Vector2(0,length/2), duration)
+	tween_rect_h.tween_property(self, "region_rect", Rect2(0,0,16,length), duration)
+ 
+func _input(event):
+	if event.is_action_pressed("action"):
+		interpolate(await check_collision(), 0.2)
+		await get_tree().create_timer(0.2).timeout
+		if not is_colling:
+			reverse_interpolate()
+	
+	if event.is_action_released("action"):
+		if is_colling:
+			reverse_interpolate()
+			un_hooked.emit()
+ 
+func reverse_interpolate():
+	interpolate(0,0.75)
+ 
+func check_collision():
+	await get_tree().create_timer(0.1).timeout
+	var collision_point
+	if ray_cast.is_colliding():
+		is_colling = true
+		collision_point = ray_cast.get_collision_point()
+		distance = (global_position - collision_point).length()
+		hooked.emit(distance, collision_point)
+	else:
+		distance = 150.0
+		is_colling = false
+	return distance * 2.5
